81a505564ff34d17e6ca0697b0aea5b1193b8bf1
m4nu-git <manudev912@gmail.com>
2026-04-13 10:12:19 +0530

    configured prisma

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from 'express';
 
 import ServerConfig from './config/server.config';
+import { PrismaClient } from '@prisma/client';
+
+export { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 const app :Express = express();
 
@@ -8,6 +12,8 @@ app.get('/ping', (req: Request, res: Response) => {
     res.json({msg: 'ok'})
 })
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Server started at PORT: ${ServerConfig.PORT}`)
+    const user = await prisma.user.findMany();
+    console.log(user)
 })
